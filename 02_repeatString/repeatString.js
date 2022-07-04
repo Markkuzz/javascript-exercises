788fe1cf21ae70079d9491a24372901fddc26f56
Markkuzz <mboncato21@gmail.com>
2022-07-04 11:15:52 +1000

    a few exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+if (number<0) return "ERROR";
+let string = '';
+for(let i=0;i<number;i++){
+    string += word;
+}
+return string;
 };
-
+console.log(repeatString('hey', -1))
 // Do not edit below this line
 module.exports = repeatString;
